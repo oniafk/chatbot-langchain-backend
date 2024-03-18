7c0da85fc289c8004f14b9c1b19a8eb0c1847122
Onimae <manufelipearias@gmail.com>
2024-03-19 02:05:52 +1100

    updated library imports

--- src/langchain/chatbot/retrievers/customerServiceRulesRetriever.ts
+++ src/langchain/chatbot/retrievers/customerServiceRulesRetriever.ts
@@ -1,18 +1,18 @@
-import { SupabaseVectorStore } from "langchain/vectorstores/supabase";
+import { SupabaseVectorStore } from "@langchain/community/vectorstores/supabase";
 import { createClient } from "@supabase/supabase-js";
 import { OpenAIEmbeddings } from "@langchain/openai";
 
-const openAIApiKey = process.env.OPENAI_API_KEY as string;
+const openAIApiKey = process.env.OPENAI_API_KEY;
 
-const embeddings = new OpenAIEmbeddings({ openAIApiKey: openAIApiKey });
-const supabaseApiKey = process.env.SUPABASE_API_KEY as string;
-const supabaseURL = process.env.SUPABASE_URL as string;
+const embeddings = new OpenAIEmbeddings({ openAIApiKey });
+const supabaseApiKey = process.env.SUPABASE_API_KEY;
+const supabaseURL = process.env.SUPABASE_URL;
 const client = createClient(supabaseURL!, supabaseApiKey!);
 
 const vectorStore = new SupabaseVectorStore(embeddings, {
   client,
   tableName: "documents", //table that contains the vectors that we want to search and match information from
-  queryName: "document_documents", //query that we want to use to search the table, this is on supabase and can be edited
+  queryName: "match_documents", //query that we want to use to search the table, this is on supabase and can be edited
 });
 
 const retriever = vectorStore.asRetriever(); // this is the retriever that we will use to search the table that is defined on the vectorStore const
--- src/langchain/database/sql/agent.ts
+++ src/langchain/database/sql/agent.ts
@@ -1,76 +1,76 @@
-import {
-  ChatPromptTemplate,
-  HumanMessagePromptTemplate,
-  MessagesPlaceholder,
-} from "@langchain/core/prompts";
-import { ChatOpenAI } from "@langchain/openai";
-import { createOpenAIToolsAgent, AgentExecutor } from "langchain/agents";
-import { SqlToolkit } from "langchain/agents/toolkits/sql";
-import { AIMessage } from "langchain/schema";
-import { SqlDatabase } from "langchain/sql_db";
-import { DataSource } from "typeorm";
+// import {
+//   ChatPromptTemplate,
+//   HumanMessagePromptTemplate,
+//   MessagesPlaceholder,
+// } from "@langchain/core/prompts";
+// import { ChatOpenAI } from "@langchain/openai";
+// import { createOpenAIToolsAgent, AgentExecutor } from "langchain/agents";
+// import { SqlToolkit } from "langchain/agents/toolkits/sql";
+// import { AIMessage } from "langchain/schema";
+// import { SqlDatabase } from "langchain/sql_db";
+// import { DataSource } from "typeorm";
 
-const DataBase = process.env.DATABASE_URL as string;
+// const DataBase = process.env.DATABASE_URL as string;
 
-export const agentLLM = async () => {
-  const datasource = new DataSource({
-    type: "mysql",
-    url: DataBase,
-    host: "planetscale",
-    ssl: {
-      rejectUnauthorized: true,
-    },
-    database: "onimaedb",
-  });
+// export const agentLLM = async () => {
+//   const datasource = new DataSource({
+//     type: "mysql",
+//     url: DataBase,
+//     host: "planetscale",
+//     ssl: {
+//       rejectUnauthorized: true,
+//     },
+//     database: "onimaedb",
+//   });
 
-  const db = await SqlDatabase.fromDataSourceParams({
-    appDataSource: datasource,
-  });
-  const llm = new ChatOpenAI({ modelName: "gpt-3.5-turbo", temperature: 0 });
-  const sqlToolKit = new SqlToolkit(db, llm);
-  const tools = sqlToolKit.getTools();
-  const SQL_PREFIX = `You are an agent designed to interact with a SQL database.
-      Given an input question, create a syntactically correct {dialect} query to run, then look at the results of the query and return the answer.
-      Unless the user specifies a specific number of examples they wish to obtain, always limit your query to at most {top_k} results using the LIMIT clause.
-      You can order the results by a relevant column to return the most interesting examples in the database.
-      Never query for all the columns from a specific table, only ask for a the few relevant columns given the question.
-      You have access to tools for interacting with the database.
-      Only use the below tools.
-      Only use the information returned by the below tools to construct your final answer.
-      You MUST double check your query before executing it. If you get an error while executing a query, rewrite the query and try again.
-      
-      DO NOT make any DML statements (INSERT, UPDATE, DELETE, DROP etc.) to the database.
-      
-      If the question does not seem related to the database, just return "I don't know" as the answer.`;
-  const SQL_SUFFIX = `Begin!
-      
-      Question: {input}
-      Thought: I should look at the tables in the database to see what I can query.
-      {agent_scratchpad}`;
-  const prompt = ChatPromptTemplate.fromMessages([
-    ["system", SQL_PREFIX],
-    HumanMessagePromptTemplate.fromTemplate("{input}"),
-    new AIMessage(SQL_SUFFIX.replace("{agent_scratchpad}", "")),
-    new MessagesPlaceholder("agent_scratchpad"),
-  ]);
-  const newPrompt = await prompt.partial({
-    dialect: sqlToolKit.dialect,
-    top_k: "10",
-  });
-  const runnableAgent = await createOpenAIToolsAgent({
-    llm,
-    tools,
-    prompt: newPrompt,
-  });
-  const agentExecutor = new AgentExecutor({
-    agent: runnableAgent,
-    tools,
-  });
+//   const db = await SqlDatabase.fromDataSourceParams({
+//     appDataSource: datasource,
+//   });
+//   const llm = new ChatOpenAI({ modelName: "gpt-3.5-turbo", temperature: 0 });
+//   const sqlToolKit = new SqlToolkit(db, llm);
+//   const tools = sqlToolKit.getTools();
+//   const SQL_PREFIX = `You are an agent designed to interact with a SQL database.
+//       Given an input question, create a syntactically correct {dialect} query to run, then look at the results of the query and return the answer.
+//       Unless the user specifies a specific number of examples they wish to obtain, always limit your query to at most {top_k} results using the LIMIT clause.
+//       You can order the results by a relevant column to return the most interesting examples in the database.
+//       Never query for all the columns from a specific table, only ask for a the few relevant columns given the question.
+//       You have access to tools for interacting with the database.
+//       Only use the below tools.
+//       Only use the information returned by the below tools to construct your final answer.
+//       You MUST double check your query before executing it. If you get an error while executing a query, rewrite the query and try again.
 
-  console.log(
-    await agentExecutor.invoke({
-      input:
-        "can you tell me whast the order that has the highest price and the details of it?",
-    })
-  );
-};
+//       DO NOT make any DML statements (INSERT, UPDATE, DELETE, DROP etc.) to the database.
+
+//       If the question does not seem related to the database, just return "I don't know" as the answer.`;
+//   const SQL_SUFFIX = `Begin!
+
+//       Question: {input}
+//       Thought: I should look at the tables in the database to see what I can query.
+//       {agent_scratchpad}`;
+//   const prompt = ChatPromptTemplate.fromMessages([
+//     ["system", SQL_PREFIX],
+//     HumanMessagePromptTemplate.fromTemplate("{input}"),
+//     new AIMessage(SQL_SUFFIX.replace("{agent_scratchpad}", "")),
+//     new MessagesPlaceholder("agent_scratchpad"),
+//   ]);
+//   const newPrompt = await prompt.partial({
+//     dialect: sqlToolKit.dialect,
+//     top_k: "10",
+//   });
+//   const runnableAgent = await createOpenAIToolsAgent({
+//     llm,
+//     tools,
+//     prompt: newPrompt,
+//   });
+//   const agentExecutor = new AgentExecutor({
+//     agent: runnableAgent,
+//     tools,
+//   });
+
+//   console.log(
+//     await agentExecutor.invoke({
+//       input:
+//         "can you tell me whast the order that has the highest price and the details of it?",
+//     })
+//   );
+// };
