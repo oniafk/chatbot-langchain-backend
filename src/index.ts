429184defa6a05be507673c54ac599ede67776f1
Onimae <manufelipearias@gmail.com>
2024-03-19 02:05:09 +1100

    added route for getting request from client

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,19 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 
-import { db } from "./langchain/database/sql/index";
-import { agentLLM } from "./langchain/database/sql/agent";
+// import { db } from "./langchain/database/sql/index";
+// import { agentLLM } from "./langchain/database/sql/agent";
 import { createUser } from "../prisma/controllers/user.controller";
 import { createPaymentMethod } from "../prisma/controllers/paymentMethod.controller";
 import { createProduct } from "../prisma/controllers/product.controller";
 import { createCarrier } from "../prisma/controllers/carrier.controller";
 import { createStatusDelivery } from "../prisma/controllers/statusDelivery.controller";
 import { createOrder } from "../prisma/controllers/order.controller";
+import ChainSequence from "./langchain/chatbot/ChainSequence";
 
 const app = new Hono();
 
 app.get("/", (c) => {
-  db();
-  agentLLM();
   return c.text("Hello Hono!");
 });
 
@@ -24,6 +23,7 @@ app.post("/products", createProduct);
 app.post("/carriers", createCarrier);
 app.post("/status-deliveries", createStatusDelivery);
 app.post("/create-order", createOrder);
+app.post("/chatbot", ChainSequence);
 
 const port = 3000;
 console.log(`Server is running on port ${port}`);
