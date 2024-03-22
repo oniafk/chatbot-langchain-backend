2a1e6ea058b168d5d0a35d906cd5616324a27315
Onimae <manufelipearias@gmail.com>
2024-03-22 13:19:18 +1100

    created textLoader, textSplitter for what no to do on a conversation for llm

---
+++ src/langchain/documents/customerService/NosScenarios/textLoader.ts
@@ -0,0 +1,15 @@
+import { DirectoryLoader } from "langchain/document_loaders/fs/directory";
+import { TextLoader } from "langchain/document_loaders/fs/text";
+
+const loader = new DirectoryLoader(
+  "src/langchain/documents/customerService/NosScenarios/",
+  {
+    ".txt": (path) => new TextLoader(path),
+  }
+);
+
+export default async function loadText() {
+  const text = await loader.load();
+  console.log(text);
+  return { text };
+}
