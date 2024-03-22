40a31995c9214258d8af74b2222f9798358c57ec
Onimae <manufelipearias@gmail.com>
2024-03-22 13:21:23 +1100

    created textLoader, textSplitter for company policies for llm of thing that is allow to do and solutions for customer

---
+++ src/langchain/documents/customerService/customerServicePolicy/textLoader.ts
@@ -0,0 +1,11 @@
+import { DirectoryLoader } from "langchain/document_loaders/fs/directory";
+import { TextLoader } from "langchain/document_loaders/fs/text";
+
+const loader = new DirectoryLoader("src/lib/text-embedding/", {
+  ".txt": (path) => new TextLoader(path),
+});
+
+export default async function loadText() {
+  const text = await loader.load();
+  return { text };
+}
---
+++ src/langchain/documents/customerService/customerServicePolicy/textSplitter.ts
@@ -0,0 +1,26 @@
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import loadText from "./textLoader";
+import { Document } from "@langchain/core/documents";
+// verify text Splitter notes and improve the code
+const getSplittedText = async () => {
+  try {
+    const { text } = await loadText();
+    const result = text[0].pageContent;
+
+    const splitter = new RecursiveCharacterTextSplitter({
+      chunkSize: 500,
+      chunkOverlap: 50,
+      separators: [" ", "\n"],
+    });
+
+    const docOutput = await splitter.splitDocuments([
+      new Document({ pageContent: result }),
+    ]);
+
+    return docOutput;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default getSplittedText;
