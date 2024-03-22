a7a207ee57b95924d188791fa82df3fb5f9b1774
Onimae <manufelipearias@gmail.com>
2024-03-22 13:20:06 +1100

    created textLoader, textSplitter for how to behave on a conversarion with a customer for llm

---
+++ src/langchain/documents/customerService/customerServiceAsociate/textSplitter.ts
@@ -0,0 +1,27 @@
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import loadText from "./textLoader";
+import { Document } from "@langchain/core/documents";
+
+const getSplittedText = async () => {
+  try {
+    const { text } = await loadText();
+    const result = text[0].pageContent;
+
+    const splitter = new RecursiveCharacterTextSplitter({
+      chunkSize: 800,
+      chunkOverlap: 50,
+      separators: ["#", "\n"],
+    });
+
+    const docOutput = await splitter.splitDocuments([
+      new Document({ pageContent: result }),
+    ]);
+
+    console.log(docOutput);
+    return docOutput;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default getSplittedText;
