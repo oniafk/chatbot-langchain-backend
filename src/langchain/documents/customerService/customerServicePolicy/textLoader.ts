4e12f592a7d8407a66b07804e2687f7715498909
Onimae <manufelipearias@gmail.com>
2024-04-05 22:35:18 +1100

    changed export and structure of document for company policies

--- src/langchain/documents/customerService/customerServicePolicy/textLoader.ts
+++ src/langchain/documents/customerService/customerServicePolicy/textLoader.ts
@@ -1,9 +1,12 @@
 import { DirectoryLoader } from "langchain/document_loaders/fs/directory";
 import { TextLoader } from "langchain/document_loaders/fs/text";
 
-const loader = new DirectoryLoader("src/lib/text-embedding/", {
-  ".txt": (path) => new TextLoader(path),
-});
+const loader = new DirectoryLoader(
+  "src/langchain/documents/customerService/customerServicePolicy/",
+  {
+    ".txt": (path) => new TextLoader(path),
+  }
+);
 
 export default async function loadText() {
   const text = await loader.load();
