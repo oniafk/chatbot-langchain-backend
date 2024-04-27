bcec8b95feb2076b588538fbc7379f2b81e7e6fe
Onimae <manufelipearias@gmail.com>
2024-04-27 17:47:20 +1000

    added new document for handeling consecions with specific cases of orders and loaded to vector store

---
+++ src/langchain/documents/customerService/shipments/testLoader.ts
@@ -0,0 +1,15 @@
+import { DirectoryLoader } from "langchain/document_loaders/fs/directory";
+import { TextLoader } from "langchain/document_loaders/fs/text";
+
+const loader = new DirectoryLoader(
+  "src/langchain/documents/customerService/shipments/",
+  {
+    ".txt": (path) => new TextLoader(path),
+  }
+);
+
+export default async function loadText() {
+  const text = await loader.load();
+
+  return { text };
+}
