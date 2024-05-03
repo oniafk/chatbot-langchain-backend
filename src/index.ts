606dd45511e00980668ee0f7bf2844878a82b207
Onimae <manufelipearias@gmail.com>
2024-05-03 12:02:15 +1000

    updating packages for deplyo

--- src/index.ts
+++ src/index.ts
@@ -47,10 +47,12 @@ app.get("/orders", getOrders);
 app.get("/payment-methods", getPaymentMethods);
 app.get("/all-order-info", getAllOrders);
 
-const port: number = process.env.PORT ? parseInt(process.env.PORT) : 3001;
-console.log(`Server is running on port ${port}`);
+const port = parseInt(process.env.PORT!) || 3001;
+console.log(`Running at http://localhost:${port}`);
+// const port = 3001;
+// console.log(`Server is running on port ${port}`);
 
-serve({
-  fetch: app.fetch,
+export default {
   port,
-});
+  fetch: app.fetch,
+};
