966ab769c7db3bc5e4188182889c434c71d66341
Onimae <manufelipearias@gmail.com>
2024-06-17 18:28:39 +1000

    changed port for deploy

--- src/index.ts
+++ src/index.ts
@@ -57,7 +57,7 @@ app.get("/orders", getOrders);
 app.get("/payment-methods", getPaymentMethods);
 app.get("/all-order-info", getAllOrders);
 
-const port = parseInt(process.env.PORT! || "3001");
+const port = process.env.PORT! || 4000;
 console.log(`Running at http://localhost:${port}`);
 // const port = 3001;
 // console.log(`Server is running on port ${port}`);
@@ -69,5 +69,5 @@ export default {
 
 serve({
   fetch: app.fetch,
-  port: 3001,
+  port: 4000,
 });
