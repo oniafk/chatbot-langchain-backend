fc83852949c5f381a703738ed9fbe3dfe211c8cc
Onimae <manufelipearias@gmail.com>
2024-06-17 20:31:11 +1000

    test deploy get end point

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client";
-import getRelevantDocuments from "./langchain/chatbot/retrievers/matryoshkaRetriever";
+import matrtyoshkaApp from "./langchain/chatbot/retrievers/matryoshkaRetriever";
 const app = new Hono();
 
 const prisma = new PrismaClient();
@@ -10,32 +10,32 @@ app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
 
-// app.get("/all-order-info", async (c) => {
-//   try {
-//     const orders = await prisma.order.findMany({
-//       where: {
-//         order_user_id: 1,
-//       },
-//       include: {
-//         order_carrier: {
-//           select: {
-//             carrier_name: true,
-//           },
-//         },
-//         order_status: {
-//           select: {
-//             status_delivery_name: true,
-//           },
-//         },
-//       },
-//     });
+app.get("/all-order-info", async (c) => {
+  try {
+    const orders = await prisma.order.findMany({
+      where: {
+        order_user_id: 1,
+      },
+      include: {
+        order_carrier: {
+          select: {
+            carrier_name: true,
+          },
+        },
+        order_status: {
+          select: {
+            status_delivery_name: true,
+          },
+        },
+      },
+    });
 
-//     return c.json({ orders, ok: true });
-//   } catch (error) {
-//     console.error(error);
-//     return c.json({ error: "An error occurred while fetching orders" }, 500);
-//   }
-// });
+    return c.json({ orders, ok: true });
+  } catch (error) {
+    console.error(error);
+    return c.json({ error: "An error occurred while fetching orders" }, 500);
+  }
+});
 
 // app.post("/chatbot", getRelevantDocuments);
 
